035b6aa4bf45ad4b07d709d1a3154342a20cf6e8
Ashley Davis <ashley@codecapers.com.au>
2021-07-21 20:25:28 +1000

    Removed dist files that shouldn't be included.

--- test/dist/index.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var my_library_1 = require("../../my-library");
-my_library_1.showMessage();
--- test/test-project/my-library/dist/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.showMessage = void 0;
-function showMessage() {
-    console.log("Hello world!\n");
-}
-exports.showMessage = showMessage;
